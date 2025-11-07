ae0233a024899c2f41f4c225dfe070d135b3b794
bestfizzz <minhhoangnguyen19122004@gmail.com>
2025-11-07 09:06:41 +0700

    remove bg in admin

--- components/layout-admin.tsx
+++ components/layout-admin.tsx
@@ -12,15 +12,6 @@ export default function AdminLayout({
             <AppSidebar />
             <SidebarInset className="relative overflow-hidden">
                 <Header />
-                <div
-                    className="bg-[length:12rem_6rem] sm:bg-[length:16rem_8rem] lg:bg-[length:21rem_9rem] absolute left-1/2 top-1/2 opacity-4 w-[200%] h-[250%] -translate-x-1/2 -translate-y-1/2"
-                    style={{
-                        backgroundImage: "url(/logo/small-logo-netnam-padding.png)",
-                        backgroundRepeat: "repeat space",
-                        transform: "rotate(45deg)",
-                        transformOrigin: "center",
-                    }}
-                />
                 {children}
             </SidebarInset>
         </SidebarProvider>
